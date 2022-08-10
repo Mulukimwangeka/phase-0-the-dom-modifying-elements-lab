498e15461d3016b3e832dfe443b14bcbc68f8483
mulukimwangeka <mwangekaruth@gmail.com>
2022-08-10 12:41:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const newHeader = document.createElement('H1')
+document.body.appendChild(newHeader)
+newHeader.setAttribute("id", 'victory');
+
+// const firstName = "Ruth";
+
+newHeader.innerHTML = `Ruth is the champion`;
